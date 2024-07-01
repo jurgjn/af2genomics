0e0c1a468a79e6bbc1371e27758190719b9fee6b
jurgjn <jurgjn@users.noreply.github.com>
2024-07-01 13:32:20 +0200

    Show login information for code-server

--- software/code-server-ssh.sh
+++ software/code-server-ssh.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 CODE_SERVER_NODE=`ssh euler.ethz.ch 'squeue --me --name=code-server-gpu --noheader --format="%R"'`
-echo "Starting tunnel to $CODE_SERVER_NODE"
+echo "Starting tunnel to $CODE_SERVER_NODE; passphrase:"
+ssh euler.ethz.ch 'cat .config/code-server/config.yaml'
 ssh euler.ethz.ch -N -L 59123:$CODE_SERVER_NODE.euler.ethz.ch:8898
